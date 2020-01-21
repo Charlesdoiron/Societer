9befcf11e25a97ce78aee24bd907b766e88994bb
charlesdoiron <doironcharles@gmail.com>
2020-01-21 19:59:40 +0100

    remove rsx

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Menu from "./menu";
 import Socials from "./socials";
+import { useRouter } from "next/router";
 const Layout = props => {
+  const router = useRouter();
+  const currentPage = router.pathname;
   return (
     <>
       <Menu />
-      <Socials />
+      {currentPage !== "/contact" && <Socials />}
+
       {props.children}
     </>
   );
